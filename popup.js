6e794c174c2e599e6e912476d2a1f6eaf123de11
Your Name <maksimladyckuk@gmail.com>
2024-05-15 19:58:52 +0300

    first commit

---
+++ popup.js
@@ -0,0 +1,28 @@
+    let popupBg = document.querySelector('.popup__bg');
+let popup = document.querySelector('.popup');
+let openPopupBtn = document.querySelectorAll('.open-popup');
+let closePopupBtn = document.querySelector('.close-popup');
+
+
+openPopupBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBg.classList.add('active');
+        popup.classList.add('active');
+
+        
+    })
+});
+
+closePopupBtn.addEventListener('click',() => {
+    popupBg.classList.remove('active');
+    popup.classList.remove('active');
+});
+
+document.addEventListener('click', (e) => {
+    if(e.target === popupBg) {
+        popupBg.classList.remove('active');
+        popup.classList.remove('active');
+    }
+});
+    
